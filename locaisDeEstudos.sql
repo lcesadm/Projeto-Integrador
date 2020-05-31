6ebd10e8d4d8d145daebf7816a897c62d7ad0b87
Jason ALves <jasonalves2008@hotmail.com>
2020-05-31 14:37:01 -0300

    adicionando arquivo com locais de estudos

---
+++ locaisDeEstudos.sql
@@ -0,0 +1,10 @@
+SELECT * FROM learning_map.mapas;
+
+
+INSERT INTO `mapas` (`id_mapa`, `nome`, `endereco`, `latitude`, `longitude`, `tipo`,`createdAt`,`updatedAt`) VALUES ('1', 'Google Space - Campus SP', ' Rua Coronel Oscar Porto, 70 - Paraíso, São Paulo - SP, 04003-000', '-23.570804', '-46.649875', 'empresa',NOW(),NOW());
+INSERT INTO `mapas` (`id_mapa`, `nome`, `endereco`, `latitude`, `longitude`, `tipo`,`createdAt`,`updatedAt`) VALUES ('2', 'Cubo Itaú', 'Alameda Vicente Pinzon, 54 - Vila Olímpia, São Paulo - SP, 04547-130', '-23.596431', '-46.686795', 'empresa',NOW(),NOW());
+INSERT INTO `mapas` (`id_mapa`, `nome`, `endereco`, `latitude`, `longitude`, `tipo`,`createdAt`,`updatedAt`) VALUES ('3', 'Coworking Spaces FIAP SU – Paulista', 'Av. Paulista, 1106 - 7º andar - Cerqueira César, São Paulo - SP, 01311-000', '-23.563837', '-46.652427', 'universidade',NOW(),NOW());
+INSERT INTO `mapas` (`id_mapa`, `nome`, `endereco`, `latitude`, `longitude`, `tipo`,`createdAt`,`updatedAt`) VALUES ('4', 'Biblioteca Mário de Andrade', 'R. da Consolação, 94 - República, São Paulo - SP, 01302-000', '-23.547192', '-46.642054', 'biblioteca',NOW(),NOW());
+INSERT INTO `mapas` (`id_mapa`, `nome`, `endereco`, `latitude`, `longitude`, `tipo`,`createdAt`,`updatedAt`) VALUES ('5', 'Centro Cultural de São Paulo – Paraíso', 'Rua Vergueiro, 1000 - Paraíso, São Paulo - SP, 01504-000', '-23.570879', '-46.640226', 'centro cultural',NOW(),NOW());
+INSERT INTO `mapas` (`id_mapa`, `nome`, `endereco`, `latitude`, `longitude`, `tipo`,`createdAt`,`updatedAt`) VALUES ('6', ' Le Pain Quotidien – Vila Madalena ', 'R. Wisard, 138 - Vila Madalena, São Paulo - SP, 05434-000', '-23.554340', '-46.688695', 'padaria',NOW(),NOW());
+INSERT INTO `mapas` (`id_mapa`, `nome`, `endereco`, `latitude`, `longitude`, `tipo`,`createdAt`,`updatedAt`) VALUES ('7', 'Vista Café – Vila Olímpia ', 'Av. Pres. Juscelino Kubitschek, 2041 - Vila Olímpia, São Paulo - SP, 04543-011', '-23.591249', '-46.690040', 'shopping',NOW(),NOW());
